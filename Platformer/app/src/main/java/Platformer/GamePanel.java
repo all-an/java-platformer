7cb32795433e4373d665b6731a10aea4d44cc6cf
all-an <all-an@hotmail.com>
2021-12-13 12:51:33 -0300

    learning

--- Platformer/app/src/main/java/Platformer/GamePanel.java
+++ Platformer/app/src/main/java/Platformer/GamePanel.java
@@ -4,10 +4,41 @@
  */
 package Platformer;
 
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  *
  * @author all-a
  */
-public class GamePanel extends javax.swing.JPanel {
+public class GamePanel extends javax.swing.JPanel implements ActionListener {
+    
+    Player player;
+    
+    Timer gameTimer;
+    
+    public GamePanel(){
+        gameTimer = new Timer();
+        gameTimer.schedule(new TimerTask(){
+            @Override
+            public void run() {
+            }
+            
+        },0, 17);
+    }
+    
+    public void Paint(Graphics g){
+        super.paint(g);
+        
+        Graphics2D gtd = (Graphics2D) g;
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
     
 }
