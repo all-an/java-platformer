ebe41072628f0746b427e834dd074191ce9154a5
all-an <all-an@hotmail.com>
2021-12-14 16:55:37 -0300

    leaning

--- Platformer/app/src/main/java/Platformer/Player.java
+++ Platformer/app/src/main/java/Platformer/Player.java
@@ -4,6 +4,8 @@
  */
 package Platformer;
 
+import java.awt.Rectangle;
+
 /**
  *
  * @author all-a
@@ -11,12 +13,24 @@ package Platformer;
 public class Player {
     
     GamePanel panel;
-    int x, y;
+    int x, y, width, height;
+    double xspeed, yspeed;
+    
+    Rectangle hitBox;
+    
+    boolean keyLeft, keyRight, keyUp, keyDown;
     
     public Player(int x, int y, GamePanel panel){
         this.panel = panel;
         this.x = x;
         this.y = y;
+    
+        width = 50;
+        height = 100;
+        hitBox = new Rectangle(x, y, width, height);
+        
     }
+    
+    
     
 }
