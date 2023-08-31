c93e5dbb411d2b75ae2cc3895e2ff7303b38ba2a
all-an <all-an@hotmail.com>
2023-08-31 20:09:29 -0300

    beginning keylistner part 2

---
+++ javaplatformer/src/main/java/gamecore/KeyListener.java
@@ -0,0 +1,17 @@
+package gamecore;
+
+public class KeyListener {
+
+	private static KeyListener keyListener;
+	private boolean KeyPressed[] = new boolean[350];
+
+	private KeyListener() {
+
+	}
+
+	public static void keyCallBack(long window, int key, int scanCode, int action, int mods){
+		
+	}
+
+
+}
